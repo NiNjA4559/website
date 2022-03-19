223c9f2f3845c00b21696be806920dd3ae2b1764
NiNjA4559 <89719423+NiNjA4559@users.noreply.github.com>
2022-03-19 12:41:53 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,23 @@
+const ejs = require("ejs");
+const express = require("express");
+let bodyParser = require('body-parser');
+const fs = require("fs");
+
+let app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.json());
+app.use("/styles", express.static(__dirname+'/public/styles'));
+app.use("/assets", express.static(__dirname+'/public/assets'));
+
+app.engine("html", ejs.renderFile);
+app.set("view engine", "html");
+
+app.get("/", (req, res) => {
+    res.redirect("/home");
+});
+app.get("/:code", async(req, res) => { 
+    if(!req.params) return res.redirect("/home");
+    if(req.params.code.toLowerCase() == "home") return res.render("home");
+});
+app.listen(2000)
